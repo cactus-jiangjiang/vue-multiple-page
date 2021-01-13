037f8e28865b90e7e1fa7d53294ac0749e0f4e52
jiangying <jiang_ying@staruniverse.cn>
2021-01-13 10:41:16 +0800

    第一次提交

---
+++ src/pages/test/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="test">
+    <div>test</div>
+    <a href="index.html">to Index</a>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style>
+
+</style>
